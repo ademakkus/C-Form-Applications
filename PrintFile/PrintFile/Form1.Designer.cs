728b9ead4ac2e0ada81afa0c473143e8b79816fc
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-06-01 20:54:52 +0300

    Add files via upload

---
+++ PrintFile/PrintFile/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace PrintFile
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.pageSetupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.printDialog1 = new System.Windows.Forms.PrintDialog();
+      this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+      this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+      this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.pageSetupToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.printPreviewToolStripMenuItem,
+            this.quitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+      this.fileToolStripMenuItem.Text = "&File";
+      // 
+      // pageSetupToolStripMenuItem
+      // 
+      this.pageSetupToolStripMenuItem.Name = "pageSetupToolStripMenuItem";
+      this.pageSetupToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.pageSetupToolStripMenuItem.Text = "Page Setup";
+      this.pageSetupToolStripMenuItem.Click += new System.EventHandler(this.pageSetupToolStripMenuItem_Click);
+      // 
+      // printToolStripMenuItem
+      // 
+      this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+      this.printToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.printToolStripMenuItem.Text = "&Print";
+      this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+      // 
+      // printPreviewToolStripMenuItem
+      // 
+      this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+      this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.printPreviewToolStripMenuItem.Text = "Print Preview";
+      this.printPreviewToolStripMenuItem.Click += new System.EventHandler(this.printPreviewToolStripMenuItem_Click);
+      // 
+      // openFileToolStripMenuItem
+      // 
+      this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+      this.openFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.openFileToolStripMenuItem.Text = "&Open File";
+      this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+      // 
+      // quitToolStripMenuItem
+      // 
+      this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+      this.quitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+      this.quitToolStripMenuItem.Text = "&Quit";
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // printDialog1
+      // 
+      this.printDialog1.UseEXDialog = true;
+      // 
+      // printPreviewDialog1
+      // 
+      this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+      this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+      this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+      this.printPreviewDialog1.Document = this.printDocument1;
+      this.printPreviewDialog1.Enabled = true;
+      this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+      this.printPreviewDialog1.Name = "printPreviewDialog1";
+      this.printPreviewDialog1.Visible = false;
+      // 
+      // printDocument1
+      // 
+      this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+      // 
+      // pageSetupDialog1
+      // 
+      this.pageSetupDialog1.Document = this.printDocument1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem pageSetupToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.PrintDialog printDialog1;
+    private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    private System.Drawing.Printing.PrintDocument printDocument1;
+    private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+  }
+}
+
---
+++ PrintFile/PrintFile/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintFile
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void toolStripTextBox1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void pageSetupToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      pageSetupDialog1.ShowDialog();
+    }
+    string str;
+    string[] rows;
+    private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      DialogResult openFileDialogResult;
+      openFileDialogResult = openFileDialog1.ShowDialog();
+      if (openFileDialogResult == DialogResult.OK)
+      {
+        StreamReader reader = new StreamReader(openFileDialog1.FileName);
+        str = reader.ReadToEnd();
+        reader.Close();
+        rows = str.Split('\n');
+      }
+    }
+
+    private void printToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      DialogResult result = printDialog1.ShowDialog();
+      if (result == DialogResult.OK)
+      {
+        arrayCounter = 0;
+        printDocument1.Print();
+      }
+    }
+
+    private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      arrayCounter = 0;
+      printPreviewDialog1.ShowDialog();
+    }
+    int arrayCounter = 0;
+    private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+    {
+      float leftMargin = e.MarginBounds.Left;
+      float topMargin = e.MarginBounds.Top;
+      float myLines = 0;
+      float yPosition = 0;
+      int counter = 0;
+      string currentLine;
+      myLines=e.MarginBounds.Height/this.Font.GetHeight(e.Graphics);
+      while (counter < myLines && arrayCounter <= rows.Length - 1)
+      {
+        currentLine = rows[arrayCounter];
+        yPosition = topMargin + counter * this.Font.GetHeight(e.Graphics);
+        e.Graphics.DrawString(currentLine, this.Font, Brushes.Red, leftMargin, yPosition, new StringFormat());
+        counter++;
+        arrayCounter++;
+        if (!(arrayCounter >= rows.GetLength(0) - 1)){
+          e.HasMorePages = true;
+        }
+        else
+        {
+          e.HasMorePages = false;
+        }
+      }
+
+    }
+  }
+}
