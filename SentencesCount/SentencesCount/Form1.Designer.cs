9ecd52ca40ba874ff71b886abc96a4f0909214ae
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-06-13 21:03:25 +0300

    Add files via upload

---
+++ SentencesCount/SentencesCount/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace SentencesCount
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Location = new System.Drawing.Point(30, 42);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(742, 221);
+      this.richTextBox1.TabIndex = 0;
+      this.richTextBox1.Text = "";
+      // 
+      // button1
+      // 
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+      this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+      this.button1.Location = new System.Drawing.Point(206, 299);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(263, 70);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Count Sentences";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.richTextBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ SentencesCount/SentencesCount/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SentencesCount
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string text = richTextBox1.Text;
+      char[] charArray = richTextBox1.Text.ToCharArray();
+      int counter = 0;
+      for (int i = 0; i < text.Length; i++)
+      {
+        if (text[i] == '!' || text[i] == '?' || text[i] == '.' )
+          counter++;
+      }
+
+      MessageBox.Show("There are " + counter.ToString()+" sentences.", "Sentences Count");
+    }
+  }
+}
